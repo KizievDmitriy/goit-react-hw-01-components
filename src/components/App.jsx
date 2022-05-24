e5afc768f3f492f43930d009392d8db10e33dfbf
KizievDmitriy <kizievdmitrii@gmail.com>
2022-05-24 13:58:58 +0300

    add TransactionHistory

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,8 @@ import data from './Statistics/data.json';
 import { Statistics } from './Statistics/statistics';
 import friends from './FriendList/friends.json';
 import { FriendList } from './FriendList/FriendList';
+import transactions from './TransactionHistory/transactions.json';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
 
 
 export const App = () => {
@@ -28,6 +30,7 @@ export const App = () => {
       />
       <Statistics title="Upload stats" stats={data} />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
