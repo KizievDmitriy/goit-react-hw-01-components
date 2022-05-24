17328887948c2b90c25f901329202fe15055b3dd
KizievDmitriy <kizievdmitrii@gmail.com>
2022-05-24 13:37:03 +0300

    add style

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,9 @@ import user from './Profile/user.json';
 import { Profile } from './Profile/profile';
 import data from './Statistics/data.json';
 import { Statistics } from './Statistics/statistics';
+import friends from './FriendList/friends.json';
+import { FriendList } from './FriendList/FriendList';
+
 
 export const App = () => {
   return (
@@ -9,7 +12,7 @@ export const App = () => {
       style={{
         height: '100vh',
         display: 'flex',
-        flex: 'wrap',
+        flexWrap: 'wrap',
         justifyContent: 'center',
         alignItems: 'center',
         color: '#010101'
@@ -24,6 +27,7 @@ export const App = () => {
         stats={user.stats}
       />
       <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
     </div>
   );
 };
